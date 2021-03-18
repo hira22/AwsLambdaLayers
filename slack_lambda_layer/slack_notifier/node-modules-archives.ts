089e37864e2925687792425a0cf3a010a49cdb20
hira22 <masayuki.hiraoka@ietty.co.jp>
2021-03-19 00:04:52 +0900

    fix archive script

--- slack_lambda_layer/slack_notifier/node-modules-archives.ts
+++ slack_lambda_layer/slack_notifier/node-modules-archives.ts
@@ -1,13 +1,25 @@
 import archiver from 'archiver';
 import fs from 'fs';
+import path from 'path';
 
 export class NodeModulesArchives {
     public static archive(): void {
-        const modules = './dist';
-        const output = fs.createWriteStream('./lambda_layer.zip');
+
+        const archiveDir = './nodejs'
+        const moduleDir = `${archiveDir}/node_modules`;
+        const moduleName = path.basename(path.resolve('.'));
+        
+        const dir = `${moduleDir}/${moduleName}`;
+        const indexJs = `${dir}/index.js`;
+        const packageJson = `${dir}/package.json`;
+
+        fs.mkdirSync(dir, { recursive: true });
+        fs.copyFileSync('./dist/index.js', indexJs);
+        fs.copyFileSync('./package.json', packageJson);
+        const output = fs.createWriteStream(`./${moduleName}.zip`);
         const archive = archiver('zip', {zlib: {level: 9}});
         archive.pipe(output);
-        archive.directory(modules, 'nodejs');
+        archive.directory(archiveDir, 'nodejs');
         archive.finalize();
     }
 }
